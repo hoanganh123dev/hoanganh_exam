b2fdd08eb665768fc910835445c63ca0f6aef930
Hoàng Anh <112646804+hoanganh123dev@users.noreply.github.com>
2023-09-25 13:17:32 +0700

    healcheck to finish

--- src/Examination/Examination.API/Program.cs
+++ src/Examination/Examination.API/Program.cs
@@ -1,26 +1,74 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Examination.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+         public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            string appName = typeof(Startup).Namespace;
+            var configuration = GetConfiguration();
+
+            Log.Logger = CreateSerilogLogger(configuration);
+            try
+            {
+                Log.Information("Starting web host ({ApplicationContext})...", appName);
+
+                var host = CreateHostBuilder(args).Build();
+
+                Log.Information("Apply configuration web host ({ApplicationContext})...", appName);
+
+                host.Run();
+
+                Log.Information("Started web host ({ApplicationContext})...", appName);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Program terminated unexpectedly ({ApplicationContext})!", appName);
+
+                return 1;
+            }
+
+            // Create a logger object
+            Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+            {
+                return new LoggerConfiguration()
+                    .MinimumLevel.Information()
+                    .Enrich.WithProperty("ApplicationContext", appName)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day, shared: true)
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+            }
+
+            IConfiguration GetConfiguration()
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .AddUserSecrets(Assembly.GetAssembly(typeof(Startup)));
+
+                var config = builder.Build();
+                return builder.Build();
+            }
         }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSerilog();
                 });
     }
 }
